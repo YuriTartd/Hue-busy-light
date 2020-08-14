e08b1c0a5ca1b292501f37a9a899cd73cdf14dc3
YuriTartd <69654472+YuriTartd@users.noreply.github.com>
2020-08-14 08:07:07 -0500

    Update hue.sh

--- hue.sh
+++ hue.sh
@@ -6,7 +6,7 @@ status=unknown #Default status
 apiKey= 
 hueBridge= #bridge IP address
 #Start loop
-while [[ selection -le 3 ]]
+while [ $selection -le 5 ]
   do
   clear
 #Display options/status and collect user input
@@ -15,33 +15,49 @@ while [[ selection -le 3 ]]
 
   Select from the following:
   1. Busy
-  2. Not Busy
-  3. Change Light
-  4. Exit"
+  2. Available
+  3. Beer Thirty
+  4. Off
+  5. Change Light selection
+  6. Exit"
   read -e selection
 
 #Set light red and change status to busy
-  if [[ $selection -eq 1 ]]
+  if [ $selection -eq 1 ]
     then
-      curl -s -X PUT -d '{"on": true,"bri": 150,"hue": 60,"sat": 245}' http://$hueBridge/api/$apiKey/lights/$lightNum/state > /dev/null
+      curl -s -X PUT -d '{"on": true,"bri": 100,"hue": 60,"sat": 245}' http://$hueBridge/api/$apiKey/lights/$lightNum/state > /dev/null
       echo "You are busy"
       status=Busy
     fi
 #Change light green and change status to Available
-    if [[ $selection -eq 2 ]]
-    then
-      curl -s -X PUT -d '{"on": true,"bri": 150,"hue": 25600,"sat": 254}' http://$hueBridge/api/$apiKey/lights/$lightNum/state > /dev/null
+    if [ $selection -eq 2 ]
+     then
+      curl -s -X PUT -d '{"on": true,"bri": 100,"hue": 25600,"sat": 254}' http://$hueBridge/api/$apiKey/lights/$lightNum/state > /dev/null
       echo "You are not busy"
       status=Available
     fi
+#Change light back to normal
+    if [ $selection -eq 3 ]
+      then
+	     curl -s -X PUT -d '{"on": true,"bri": 100,"hue": 8382,"sat":140}' http://$hueBridge/api/$apiKey/lights/$lightNum/state > /dev/null
+	     echo  "Beer Thirty!"
+	     status=BeerTime
+    fi
+#Turn light off
+    if [ $selection -eq 4 ]
+      then
+	      curl -s -X PUT -d '{"on": false}' http://$hueBridge/api/$apiKey/lights/$lightNum/state > /dev/null
+	      echo  "Beer Thirty!"
+	      status=Off
+    fi
 #Change light selection
-    if [[ $selection -eq 3 ]]
-    then
+    if [ $selection -eq 5 ]
+     then
       echo "Change Light?"
       read -e lightNum
     fi
 #Exit
-    if [[ $selection -eq 4 ]]
+    if [ $selection -eq 6 ]
       then echo "Exiting"
     fi
 done
